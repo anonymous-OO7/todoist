83c5040c5e46cd587263b192eb1ba71fcd06ad9e
Gaurav kumar yadav <gaurav@Gauravs-MacBook-Air.local>
2021-08-23 18:17:00 +0530

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 // import { FaInbox} from from 'react-icons/fa';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faInbox, faCalendarWeek, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+
+
+
+
 function Sidebar({ selectedTab, setSelectedTab }) {
 
 	console.log({ selectedTab });
+
+
 	return (
 		<div className="sidebar">
 
-			<div className="active" onClick={() => setSelectedTab("INBOX")}>Inbox</div>
-			<div onClick={() => setSelectedTab("TODAY")} >Today</div>
-			<div onClick={() => setSelectedTab("NEXT7")} >Next 7 Days</div>
+			<div className={selectedTab === 'INBOX' ? `active` : ''} onClick={() => setSelectedTab("INBOX")}><FontAwesomeIcon icon={faInbox} />Inbox</div>
+			<div className={selectedTab === 'TODAY' ? `active` : ''} onClick={() => setSelectedTab("TODAY")} ><FontAwesomeIcon icon={faCalendarWeek} />Today</div>
+			<div className={selectedTab === 'NEXT7' ? `active` : ''} onClick={() => setSelectedTab("NEXT7")} ><FontAwesomeIcon icon={faEnvelopeOpenText} />Next 7 Days</div>
 		</div>
 	)
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css';
 import dateFnsFormat from 'date-fns/format';
+import { addDays, isAfter, isBefore, isToday } from 'date-fns'
+
 
 
 
@@ -12,15 +14,16 @@ function formatDate(date, format, locale) {
 	return dateFnsFormat(date, format, { locale });
 }
 
-const AddTask = ({ onCancel, onAddTask }) => {
+const AddTask = ({ onAddTask, onCancel }) => {
 	const [task, setTask] = useState("");
 	const [date, setDate] = useState(null);
 	return (
 		<div className="add-task-dialog">
 			<input value={task} onChange={(event) => setTask(event.target.value)} />
 			<div className="add-task-actions">
+
 				<div className="btns-container">
-					<button disabled={!task} className="add-btn" onClick={() => { onAddTask(task); setTask("") }}>ADD</button>
+					<button disabled={!task} className="add-btn" onClick={() => { onAddTask(task, date); setTask("") }}>ADD</button>
 					<button className="cancel-btn" onClick={() => { onCancel(); setTask("") }} >CANCEL</button>
 
 				</div>
@@ -29,7 +32,11 @@ const AddTask = ({ onCancel, onAddTask }) => {
 						onDayChange={(day) => setDate(day)}
 						placeholder={`${dateFnsFormat(new Date(), FORMAT)}`}
 						formatDate={formatDate}
-						format={FORMAT} />
+						format={FORMAT}
+						dayPickerProps={
+							{ modifiers: { disabled: [{ before: new Date() }], } }
+						}
+					/>
 				</div>
 			</div>
 
@@ -43,36 +50,58 @@ const Task_Mapping = {
 	NEXT7: "Next 7 Days",
 };
 
-// const TaskItems = ({ selectedTab, tasks }) => {
+const TaskItems = ({ selectedTab, tasks }) => {
 
-// 	if (selectedTab === "NEXT7") {
-// 		return tasks.filter(
-// 			(task) => {
-// 				isAfter(task.date, new Date()) && isBefore(task.date, addDays(new Date(), 7))
-// 			}
-// 		).map((task) => (
-// 			<p>
-// 				{task.date}  {task.date} {" "}
-// 			</p>
-// 		));
-// 	}
+	console.log(selectedTab);
+	console.log(tasks);
 
 
-// 	if (selectedTab === "TODAY") {
-// 		return tasks.filter(
-// 			(task) => {
-// 				isAfter(task.date, new Date()) && isBefore(task.date, addDays(new Date(), 7))
-// 			}
-// 		).map(
-// 			(task) => (
-// 				<p>
-// 					{task.date}  {task.date} {" "}
-// 				</p>
-// 			)
-// 		)
-// 	}
+	if (selectedTab === "INBOX") {
+		return (
 
-// }
+			tasks.length > 0 ? tasks.map((task) => <div className="inbx">
+
+				<span id='spn1'>{task.text}</span>   <span>{dateFnsFormat(new Date(task.date), FORMAT)}</span>
+			</div>) : <p>NO TASKS YET</p>
+
+		)
+	}
+
+
+
+	if (selectedTab === "NEXT7") {
+		return (tasks.filter(
+			(task) => isAfter(task.date, new Date()) && isBefore(task.date, addDays(new Date(), 7))
+
+		).map((task) => (
+			<p>
+				{task.text}  {dateFnsFormat(new Date(task.date), FORMAT)} {" "}
+			</p>
+		)));
+
+	}
+
+
+	if (selectedTab === "TODAY") {
+		return tasks.filter(
+			(task) => isToday(task.date)
+		).map(
+			(task) => (
+
+
+				<div className="tdy">
+
+					<span id='spn1'>{task.text}</span>   <span>{dateFnsFormat(new Date(task.date), FORMAT)}</span>
+				</div>
+
+			)
+		);
+	}
+
+}
+
+
+
 
 function Task({ selectedTab }) {
 
@@ -80,6 +109,8 @@ function Task({ selectedTab }) {
 	const [tasks, setTasks] = useState([]);
 
 	const addNewTask = (text, date) => {
+		console.log("text :" + text);
+		console.log("date: " + date);
 		const newTaskItem = { text, date: date || new Date() }
 		setTasks([...tasks, newTaskItem]);
 	};
@@ -90,20 +121,24 @@ function Task({ selectedTab }) {
 
 			<div className="add-task-btn" onClick={() => setShowTask(!showTask)}>
 				<span className="plus">+</span>
-				<span className="add-task-text"> ADD TAASK</span>
+				<span className="add-task-text"> ADD TASK</span>
 			</div>
 
 			{showTask && <AddTask onAddTask={addNewTask} onCancel={() => setShowTask(false)} />}
 
+
+			{/* 
 			{tasks.length > 0 ? tasks.map((task) => <p>
 				{task.text}
 				{dateFnsFormat(new Date(task.date), FORMAT)}
-			</p>) : <p>NO TASKS YET</p>}
+			</p>) : <p>NO TASKS YET</p>} */}
 
-			{/* {tasks.length > 0 ? (
+
+
+			{tasks.length > 0 ? (
 				<TaskItems tasks={tasks} selectedTab={selectedTab} />
 
-			) : (<p>NO TASKS</p>)} */}
+			) : (<p>NO TASKS</p>)}
 		</div>
 
 
